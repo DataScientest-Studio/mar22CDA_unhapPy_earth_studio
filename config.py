3d834e286048c6eddc4527d65acea09872632e0a
Sébastien SIME <seb.sime@gmail.com>
2023-01-18 10:43:08 +0000

    changing file structure

--- config.py
+++ config.py
@@ -6,9 +6,11 @@ Config file for Streamlit App
 
 from member import Member
 
+PROMOTION = "Promotion Continue Data Analyst - Mars 2022"
 
 TITLE = "UnhapPy Earth"
 
+# membres du groupe
 TEAM_MEMBERS = [
     Member(name = "Olga Fedorova", 
            linkedin_url = "https://www.linkedin.com/in/olga-fedorova-665a4b63/", 
@@ -20,16 +22,15 @@ TEAM_MEMBERS = [
            linkedin_url="https://www.linkedin.com/in/nicolas-cristiano-7b8a23171/", 
            github_url="https://github.com/Nic0C")]
 
-PROMOTION = "Promotion Continue Data Analyst - Mars 2022"
 
 # liste des fichiers de données
-globales_file = '../data/unhappy_earth/temperatures_globales.csv'
-hemispheres_file = '../data/unhappy_earth/temperatures_hemispheres.csv'
-countries_file = '../data/unhappy_earth/temperatures_countries.csv'
+globales_file = './data/unhappy_earth/temperatures_globales.csv'
+hemispheres_file = './data/unhappy_earth/temperatures_hemispheres.csv'
+countries_file = './data/unhappy_earth/temperatures_countries.csv'
 
-co2_global = '../data/unhappy_earth/co2_global.csv'
-co2_countries = '../data/unhappy_earth/co2_countries.csv'
+co2_global = './data/unhappy_earth/co2_global.csv'
+co2_countries = './data/unhappy_earth/co2_countries.csv'
 
-temperatures_globales_file = '../data/unhappy_earth/temperatures_globales.csv'
-temperatures_hemispheres_file = '../data/unhappy_earth/temperatures_hemispheres.csv'
-temperatures_countries_file = '../data/unhappy_earth/temperatures_countries.csv'
+temperatures_globales_file = './data/unhappy_earth/temperatures_globales.csv'
+temperatures_hemispheres_file = './data/unhappy_earth/temperatures_hemispheres.csv'
+temperatures_countries_file = './data/unhappy_earth/temperatures_countries.csv'
